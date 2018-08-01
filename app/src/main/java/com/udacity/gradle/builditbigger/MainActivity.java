9cfa3c4407f6708f757045b5d937d39cddea5375
Chris Rodriguez <cerdevelopers@gmail.com>
2018-08-01 00:06:43 -0500

    feat: implement android library
    fix: build.gradle would not sync due to mismatched version libraries
    and sdk version.
    
    change support libraries to have same version, updated min sdk,
    target sdk, and compile sdk version. Change MainActivity to extend
    AppCompatActivity instead of ActionBarActivity.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,7 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,7 +10,7 @@ import android.widget.Toast;
 import christopher.joke.Joke;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
